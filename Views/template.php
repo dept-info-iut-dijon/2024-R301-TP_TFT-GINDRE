70bbe970428df9a8a51bd8c662ffd677e988812a
Valou0539 <valentin.gindre@gmail.com>
2024-10-11 11:21:49 +0200

    style(View): Update the layout
    - Add Material Symbols lib for the icons
    - Add a max width to the main

--- Views/template.php
+++ Views/template.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8"/>
     <link rel="stylesheet" href="public/css/main.css"/>
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:FILL@1" rel="stylesheet" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= $this->e($title) ?></title>
 </head>
@@ -16,7 +17,7 @@
     </nav>
 </header>
 <!-- #contenu -->
-<main id="contenu">
+<main id="contenu" class="max-w-7xl px-4 mx-auto">
     <?=$this->section('content')?>
 </main>
 <footer>
